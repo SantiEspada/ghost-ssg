084d6e10e6c4860fa3aa82cc6b732f6362dcb8e7
Santi Espada <hola@santiespada.dev>
2020-10-27 21:03:11 +0000

    Rename bash scripts and move them to a separater folder

--- ssg-server/generateSite.js
+++ ssg-server/generateSite.js
@@ -6,7 +6,7 @@ import { dirname } from './util';
 const __dirname = dirname(import.meta.url);
 
 export async function generateSite({ sourceUrl, destinationUrl, repoPath, outputFolder }) {
-  const scriptPath = path.resolve(__dirname, './generate.sh');
+  const scriptPath = path.resolve(__dirname, './sh/generate-site.sh');
 
   await exec(`sh ${scriptPath} ${sourceUrl} ${destinationUrl} ${repoPath} ${outputFolder}`);
 }
--- ssg-server/setupGit.js
+++ ssg-server/setupGit.js
@@ -6,7 +6,7 @@ import { dirname } from './util';
 const __dirname = dirname(import.meta.url);
 
 export async function setupGit({ repoUrl, email, folderName }) {
-  const scriptPath = path.resolve(__dirname, './setup-git.sh');
+  const scriptPath = path.resolve(__dirname, './sh/setup-git.sh');
 
   await exec(`sh ${scriptPath} ${repoUrl} ${email} ${folderName}`);
 }

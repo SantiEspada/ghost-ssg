703975bc7ed0fde743c8593eac82af71cd8c4974
Santi Espada <hola@santiespada.dev>
2020-11-09 21:44:30 +0000

    Rollback the content-url thingy

--- ssg-server/sh/generate-site.sh
+++ ssg-server/sh/generate-site.sh
@@ -4,7 +4,6 @@
 # See license at https://github.com/huangyuzhang/gui/blob/master/LICENSE
 
 # Define urls and https
-content_url=http://ghost:2368
 from_url=$1
 to_url=$2
 repo_path=$3
@@ -21,18 +20,18 @@ git pull
 rm -rf ${folder}
 
 # Copy blog content
-wget -e robots=off --recursive --no-host-directories --directory-prefix=${folder} --adjust-extension --timeout=30 --no-parent --convert-links ${content_url}/
+wget -e robots=off --recursive --no-host-directories --directory-prefix=${folder} --adjust-extension --timeout=30 --no-parent --convert-links ${from_url}/
 
 # Copy 404 page
-wget -e robots=off --no-host-directories --directory-prefix=${folder} --adjust-extension --timeout=30 --no-parent --convert-links --content-on-error --timestamping ${content_url}/404.html
+wget -e robots=off --no-host-directories --directory-prefix=${folder} --adjust-extension --timeout=30 --no-parent --convert-links --content-on-error --timestamping ${from_url}/404.html
 
 # Copy sitemaps
-wget -e robots=off --recursive --no-host-directories --directory-prefix=${folder} --adjust-extension --timeout=30 --no-parent --convert-links ${content_url}/sitemap.xsl
-wget -e robots=off --recursive --no-host-directories --directory-prefix=${folder} --adjust-extension --timeout=30 --no-parent --convert-links ${content_url}/sitemap.xml
-wget -e robots=off --recursive --no-host-directories --directory-prefix=${folder} --adjust-extension --timeout=30 --no-parent --convert-links ${content_url}/sitemap-pages.xml
-wget -e robots=off --recursive --no-host-directories --directory-prefix=${folder} --adjust-extension --timeout=30 --no-parent --convert-links ${content_url}/sitemap-posts.xml
-wget -e robots=off --recursive --no-host-directories --directory-prefix=${folder} --adjust-extension --timeout=30 --no-parent --convert-links ${content_url}/sitemap-authors.xml
-wget -e robots=off --recursive --no-host-directories --directory-prefix=${folder} --adjust-extension --timeout=30 --no-parent --convert-links ${content_url}/sitemap-tags.xml
+wget -e robots=off --recursive --no-host-directories --directory-prefix=${folder} --adjust-extension --timeout=30 --no-parent --convert-links ${from_url}/sitemap.xsl
+wget -e robots=off --recursive --no-host-directories --directory-prefix=${folder} --adjust-extension --timeout=30 --no-parent --convert-links ${from_url}/sitemap.xml
+wget -e robots=off --recursive --no-host-directories --directory-prefix=${folder} --adjust-extension --timeout=30 --no-parent --convert-links ${from_url}/sitemap-pages.xml
+wget -e robots=off --recursive --no-host-directories --directory-prefix=${folder} --adjust-extension --timeout=30 --no-parent --convert-links ${from_url}/sitemap-posts.xml
+wget -e robots=off --recursive --no-host-directories --directory-prefix=${folder} --adjust-extension --timeout=30 --no-parent --convert-links ${from_url}/sitemap-authors.xml
+wget -e robots=off --recursive --no-host-directories --directory-prefix=${folder} --adjust-extension --timeout=30 --no-parent --convert-links ${from_url}/sitemap-tags.xml
 
 # Replace original domain with final one
 LC_ALL=C find ./${folder} -type f -not -wholename *.git* -exec sed -i -e "s,${from_url},${to_url},g" {} +
